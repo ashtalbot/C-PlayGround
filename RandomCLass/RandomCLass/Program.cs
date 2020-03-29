be0d71b53a6ebaf10041a2abc83ddb18dbba2ed6
Ash <23111810+ashtalbot@users.noreply.github.com>
2020-03-28 22:26:30 -0600

    first commit

---
+++ RandomCLass/RandomCLass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace RandomCLass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            const int passwordLength = 10;
+
+            var buffer = new char[passwordLength];
+
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i] = (char)('a' + random.Next(0, 26));
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+        }
+    }
+}
