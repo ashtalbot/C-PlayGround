dfa4f86d4d94ea7807d09c10d2ffd61c39717c8a
Ash <23111810+ashtalbot@users.noreply.github.com>
2020-04-06 12:33:20 -0600

    string Summarising
    string method examples and class example

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is an example of a really really really really really long sentence.";
+            var summary = StringUtility.SummText(sentence, 25);
+            Console.WriteLine(summary);
+
+        }
+    }
+}
+    
+
+        
+
+
+    
+
