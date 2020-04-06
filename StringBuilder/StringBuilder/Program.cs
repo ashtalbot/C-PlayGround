41eb789cf9905c491f06a40551d7ef92ea25662e
Ash <23111810+ashtalbot@users.noreply.github.com>
2020-04-06 15:02:40 -0600

    text manipulation
    StringBuilder returns StringBuilder. Use for more efficient string manipulation.

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Collections.Generic;
+
+
+namespace StriBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+');
+                
+                System.Console.WriteLine(builder); 
+
+        }
+    }
+}
